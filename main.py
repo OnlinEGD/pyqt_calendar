0106373cbf5b15e6af58bb76467a886b285dd7ef
OnlinE <Online@tak.pl>
2023-05-30 05:36:07 +0200

    Color system added

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from PyQt5.QtCore import QDate
 from calendar_1 import Ui_MainWindow
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QColorDialog, QListWidgetItem
 import sqlite3
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QColor
 
 class Main(QMainWindow, Ui_MainWindow):
     def __init__(self):
@@ -11,8 +13,10 @@ class Main(QMainWindow, Ui_MainWindow):
         self.calendarWidget.clicked[QDate].connect(self.selected_date)
         self.pushButton.clicked.connect(self.set_event)
         self.pushButton_2.clicked.connect(self.get_events)
+        self.pushButton_3.clicked.connect(self.show_color_dialog)
         self.listWidget.itemClicked.connect(self.delete_item)
         self.selected_date_str = ""
+        self.color = "#ffffff"
 
     def selected_date(self, date):
         self.selected_date_str = date.toString("yyyy-MM-dd")
@@ -23,10 +27,14 @@ class Main(QMainWindow, Ui_MainWindow):
         cursor.execute('''CREATE TABLE IF NOT EXISTS events (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 event_name TEXT,
-                event_date DATE
+                event_date DATE,
+                color TEXT
             )''')
-        sql = "INSERT INTO events (event_name, event_date) VALUES (?, ?)"
-        values = (self.lineEdit.text(), self.selected_date_str)
+        sql = "INSERT INTO events (event_name, event_date, color) VALUES (?, ?, ?)"
+        if isinstance(self.color, str):
+            values = (self.lineEdit.text(), self.selected_date_str, self.color)
+        else:
+            values = (self.lineEdit.text(), self.selected_date_str, self.color.name())
         cursor.execute(sql, values)
         conn.commit()
         conn.close()
@@ -37,7 +45,8 @@ class Main(QMainWindow, Ui_MainWindow):
         cursor.execute('''CREATE TABLE IF NOT EXISTS events (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 event_name TEXT,
-                event_date DATE
+                event_date DATE,
+                color TEXT
             )''')
         cursor.execute("SELECT * FROM events")
         rows = cursor.fetchall()
@@ -45,8 +54,12 @@ class Main(QMainWindow, Ui_MainWindow):
         conn.close()
         self.listWidget.clear()
         for row in rows:
-            result = '  '.join(str(element) for element in row)
-            self.listWidget.addItem(result)
+            event_item = QListWidgetItem()
+            event_item.setText('  '.join(str(element) for element in row[0:3]))
+            color = row[3]
+            event_item.setForeground(QColor(color))
+            self.listWidget.addItem(event_item)
+
 
     def delete_item(self, item):
         id = int(tuple(item.text().split())[0])
@@ -69,6 +82,8 @@ class Main(QMainWindow, Ui_MainWindow):
         else:
             pass
 
+    def show_color_dialog(self):
+        self.color = QColorDialog.getColor()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
